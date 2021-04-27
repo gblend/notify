a9288d5d3be670c685930c978cae64fbbbea89ae
Ilochi Gabriel <gabrielilo190@gmail.com>
2021-04-27 11:56:01 +0000

    add provider class for rabbitmq wrapper

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Bschmitt\Amqp\AmqpServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -227,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Amqp' => Bschmitt\Amqp\Facades\Amqp::class,
 
     ],
 
